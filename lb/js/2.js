25918edf84671d437872c22bb650b02f47dac3cd
ejjava <84852759+ejjava@users.noreply.github.com>
2021-11-05 12:29:28 +0800

    Update 2.js

--- lb/js/2.js
+++ lb/js/2.js
@@ -1,6 +1,6 @@
 function search() {
     if (document.getElementById("search_input").value != "") {
-        window.location.href = "http://159.75.66.60:802/lb/#"+ document.getElementById("search_input").value;
+        window.location.href = "https://ejjava.github.io/lb/#"+ document.getElementById("search_input").value;
         document.getElementById("search_input").value = "";
     }
     return false;
