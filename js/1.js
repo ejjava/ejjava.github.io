cb39daf339be5577b106e2e8b94001a4d20bc79d
dzy914933408 <84852759+ejjava@users.noreply.github.com>
2022-06-18 16:55:44 +0800

    Add files via upload

---
+++ js/1.js
@@ -0,0 +1,19 @@
+function myFunction() {
+  // 声明变量
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+ 
+  // 循环遍历所有列表项，并隐藏那些与搜索查询不匹配的项
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
