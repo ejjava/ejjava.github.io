8b022e41213e734a57662b20d902b22327885af4
root <root@localhost>
2023-01-29 13:23:31 +0800

    2023-01-29 13:23:29

--- lb/js/2.js
+++
@@ -1,8 +0,0 @@
-function search() {
-    if (document.getElementById("search_input").value != "") {
-        window.location.href = "https://ejjava.github.io/lb/#"+ document.getElementById("search_input").value;
-        document.getElementById("search_input").value = "";
-    }
-    return false;
-}
-
