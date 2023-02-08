b680f6c46ed3c57a9ccdd15b0ad3432e2dae3adf
root <root@localhost>
2023-02-08 13:26:02 +0800

    2023-02-08 13:25:58

--- kbn/autoload.js
+++ kbn/autoload.js
@@ -1,7 +1,31 @@
-const live2d_path="./kbn/";function loadExternalResource(url,type){return new Promise((resolve,reject)=>{let tag;if(type==="css"){tag=document.createElement("link");tag.rel="stylesheet";tag.href=url;}
-else if(type==="js"){tag=document.createElement("script");tag.src=url;}
-if(tag){tag.onload=()=>resolve(url);tag.onerror=()=>reject(url);document.head.appendChild(tag);}});}
-Promise.all([loadExternalResource(live2d_path+"waifu1.css","css"),loadExternalResource(live2d_path+"live2d.min.js","js"),loadExternalResource(live2d_path+"waifu-tips.js","js")]).then(()=>{initWidget({waifuPath:live2d_path+"waifu-tips.json",cdnPath:"https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"});});console.log(`
+const live2d_path = "./kbn/";
+
+function loadExternalResource(url, type) {
+    return new Promise((resolve, reject) => {
+        let tag;
+        if (type === "css") {
+            tag = document.createElement("link");
+            tag.rel = "stylesheet";
+            tag.href = url;
+        } else if (type === "js") {
+            tag = document.createElement("script");
+            tag.src = url;
+        }
+        if (tag) {
+            tag.onload = () => resolve(url);
+            tag.onerror = () => reject(url);
+            document.head.appendChild(tag);
+        }
+    });
+}
+Promise.all([loadExternalResource(live2d_path + "waifu1.css", "css"), loadExternalResource(live2d_path + "live2d.min.js", "js"), loadExternalResource(live2d_path + "waifu-tips.js", "js")]).then(() => {
+    initWidget({
+        waifuPath: live2d_path + "waifu-tips.json",
+        cdnPath: "https://api.zsq.im/live2d"
+        //https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/
+    });
+});
+console.log(`
   く__,.ヘヽ.        /  ,ー､ 〉
            ＼ ', !-─‐-i  /  /´
            ／｀ｰ'       L/／｀ヽ､
