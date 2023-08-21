e97549cd9a43782f68be385134e3fdca837622b4
root <root@localhost>
2023-08-21 12:02:23 +0800

    2023-08-21 12:02:19

--- js/sitetime.js
+++ js/sitetime.js
@@ -1,28 +1,25 @@
-//运行时间
+// 运行时间
 function siteTime() {
   window.setTimeout("siteTime()", 1000);
-     var seconds = 1000;
-     var minutes = seconds * 60;
-     var hours = minutes * 60;
-     var days = hours * 24;
-     var years = days * 365;
-     var today = new Date();
-     var todayYear = today.getFullYear();
-     var todayMonth = today.getMonth() + 1;
-     var todayDate = today.getDate();
-     var todayHour = today.getHours();
-     var todayMinute = today.getMinutes();
-     var todaySecond = today.getSeconds();
+  var seconds = 1000;
+  var minutes = seconds * 60;
+  var hours = minutes * 60;
+  var days = hours * 24;
+  var today = new Date();
+  var todayYear = today.getFullYear();
+  var todayMonth = today.getMonth() + 1;
+  var todayDate = today.getDate();
+  var todayHour = today.getHours();
+  var todayMinute = today.getMinutes();
+  var todaySecond = today.getSeconds();
 
-
-     var t1 = Date.UTC(2000, 3, 3, 0, 00, 00); 
-     var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond);
-     var diff = t2 - t1;
-     var diffYears = Math.floor(diff / years);
-      var diffDays = Math.floor((diff / days) - diffYears * 365);
-     var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours);
-     var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes);
-     var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes * minutes) / seconds);
-         document.getElementById("sitetime").innerHTML = "已运行 " +   diffDays + " 天 " + diffHours + " 小时 " + diffMinutes + " 分钟 " + diffSeconds + " 秒";
-         }  
-         siteTime();
+  var t1 = Date.UTC(2021, 10, 15, 00, 00, 00); // 修改为指定的日期和时间，月份从0开始计数，11表示12月
+  var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond);
+  var diff = t2 - t1;
+  var diffDays = Math.floor(diff / days); // 计算相差的天数
+  var diffHours = Math.floor((diff % days) / hours); // 计算相差的小时数
+  var diffMinutes = Math.floor((diff % hours) / minutes); // 计算相差的分钟数
+  var diffSeconds = Math.floor((diff % minutes) / seconds); // 计算相差的秒数
+  document.getElementById("sitetime").innerHTML = "已运行 " + diffDays + " 天 " + diffHours + " 小时 " + diffMinutes + " 分钟 " + diffSeconds + " 秒";
+}
+siteTime();
